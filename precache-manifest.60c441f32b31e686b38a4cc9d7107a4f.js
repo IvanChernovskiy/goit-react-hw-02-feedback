4004d92083241274955ec1b9f905ccfc1849a428
Ivan Chernovkiy <vano322776@gmail.com>
2020-03-29 17:12:21 +0300

    Updates

---
+++ precache-manifest.60c441f32b31e686b38a4cc9d7107a4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b38c1a256df7f1b49d8",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime~main.2ac781c1.js"
+  },
+  {
+    "revision": "8bbe8079f5f4e93b1749",
+    "url": "/goit-react-hw-02-feedback/static/js/main.6e96cb9b.chunk.js"
+  },
+  {
+    "revision": "eb373a7d23ad15e53782",
+    "url": "/goit-react-hw-02-feedback/static/js/2.ed6e3730.chunk.js"
+  },
+  {
+    "revision": "8bbe8079f5f4e93b1749",
+    "url": "/goit-react-hw-02-feedback/static/css/main.7cc10b20.chunk.css"
+  },
+  {
+    "revision": "8150fe874a9da53abf6cd607a35bcf0a",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  }
+];
